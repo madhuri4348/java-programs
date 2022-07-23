0cd57f874bb525a22b0904a77b2f3fb45f754091
madhu <madhuri4348>
2022-07-23 15:09:01 +0530

    abcd

---
+++ program1.java
@@ -0,0 +1,28 @@
+class Animal
+{
+String name = "Dog";
+Char gender = "Female";
+String color ="Black";
+void eat()
+{
+	System.out.println(3 times per day");
+}
+void sleep()
+{
+	System.out.println("6 hours per day");
+}
+void bark()
+{
+	System.out.println("30 times  per day");
+}
+	public static void main(String args[])
+	{
+		Animal obj=new Animal();
+		obj.name;
+		obj.gender;
+		obj.color;
+		obj.eat();
+		obj.sleep();
+		obj.bark();
+	}
+}
